55cfc53eb7e7efe38ada2d988d7f3854ecf97ed4
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 12:10:04 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,8 +1,10 @@
 class Players
   class Human < Player
 
-      def move
-
+      def move(board)
+        puts "What move do you wanna make?"
+        input = gets.strip
+        board.update(input, self)
       end
 
   end
