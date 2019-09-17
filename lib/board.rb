48f1e2d956f837c36f4b2399cfdaae12400ceb1f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 11:21:49 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -19,7 +19,7 @@ class Board
   end
 
   def position(input)
-    @cells[input-1]
+    @cells[input.to_i-1]
   end
 
   def full?
@@ -32,16 +32,16 @@ class Board
   end
 
   def taken?(index)
-    !self.position(index) == " "
+    !self.position(index.to_i) == " "
   end
 
   def valid_move?(input)
-    input >= 1 && input <= 9
+    input.to_i >= 1 && input.to_i <= 9
   end
 
   def update(index, token)
     if self.valid_move?(index) && !taken?(index)
-      @cells[index-1] = token
+      @cells[index.to_i-1] = token
     end
   end
 end
